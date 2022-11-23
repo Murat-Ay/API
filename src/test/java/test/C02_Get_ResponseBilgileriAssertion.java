45318867ba2ba571bdfda1a48b421b3130e7770f
aymurat111 <aymurat111@gmail.com>
2022-11-23 20:36:12 +0100

    API

--- src/test/java/test/C02_Get_ResponseBilgileriAssertion.java
+++ src/test/java/test/C02_Get_ResponseBilgileriAssertion.java
@@ -4,15 +4,14 @@ import io.restassured.response.Response;
 import org.junit.Test;
 import static io.restassured.RestAssured.given;
 public class C02_Get_ResponseBilgileriAssertion {
-    /*
-            https://restful-booker.herokuapp.com/booking/10 url’ine bir GET request
+
+       /*   https://restful-booker.herokuapp.com/booking/10 url’ine bir GET request
             gonderdigimizde donen Response’un,
             status code’unun 200,
             ve content type’inin application/json; charset=utf-8,
             ve Server isimli Header’in degerinin Cowboy,
             ve status Line’in HTTP/1.1 200 OK
-            oldugunu test edin.
-             */
+            oldugunu test edin.           */
     @Test
     public void get02(){
         // 1- Request icin Url ve Body hazirla
