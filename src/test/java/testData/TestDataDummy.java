2a90037ddaf9328ef924574c0b40ace4053f230b
aymurat111 <aymurat111@gmail.com>
2022-12-03 13:14:40 +0100

    api

---
+++ src/test/java/testData/TestDataDummy.java
@@ -0,0 +1,27 @@
+package testData;
+import jdk.security.jarsigner.JarSigner;
+import org.json.JSONObject;
+public class TestDataDummy {
+
+
+    public JSONObject innerDataJsonBodyOlustur (){
+        JSONObject innerDataBodyJSON = new JSONObject();
+        innerDataBodyJSON.put("id",3);
+        innerDataBodyJSON.put("employee_name","Ashton Cox");
+        innerDataBodyJSON.put("employee_salary",86000);
+        innerDataBodyJSON.put("employee_age",66);
+        innerDataBodyJSON.put("profile_image","");
+        return innerDataBodyJSON;
+    }
+
+    public JSONObject expectedDataBodyOlustur (){
+
+        JSONObject expDataJSON = new JSONObject();
+
+        expDataJSON.put("status","success");
+        expDataJSON.put("data",innerDataJsonBodyOlustur());
+        expDataJSON.put("message","Successfully! Record has been fetched.");
+        return expDataJSON;
+    }
+
+}
